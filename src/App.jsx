17e52933d113a273085ffe82109ca0779ba21b3f
JoregeSosa <joregesosa@gmail.com>
2023-06-14 08:17:13 -0600

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Nav } from "./Nav";
+import { Header } from "./Header";
 
 function App() {
   // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
@@ -28,6 +29,7 @@ function App() {
   return (
     <React.Fragment>
       <Nav/>
+      <Header/>
     </React.Fragment>
      
   );
---
+++ src/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css'
+
+export function Header(props) {
+    return (
+        <header>
+            <h1>Stays in Finland</h1>
+            <h2>12+ stays</h2>
+        </header>
+    )
+}
